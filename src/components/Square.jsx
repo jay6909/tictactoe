58b61a960d07cce21ec0fa2fb2e923a86e931afe
Jayaditya <gaikwadjayaditya@gmail.com>
2022-12-31 17:55:35 +0530

    added game winner mechanics and lift up dtate

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 //import "./Square.scss"
 const Square = ({value, onClick}) => {
   
-  const clickHandler=()=>{
+  // const clickHandler=(value,onClick)=>{
 
-  }
+  // }
   return (
     <button type="button"  className="square" onClick={onClick}>{value}</button>
-  )
-}
+  );
+};
 
 export default Square
